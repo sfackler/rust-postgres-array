e28ac0405c7e7e94c152994613755999d57c74e1
Steven Fackler <sfackler@gmail.com>
2015-09-19 12:21:09 -0700

    Release v0.5.1

---
+++ doc/v0.5.1/implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres_array'] = ["impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='postgres_array/array/struct.Array.html' title='postgres_array::array::Array'>Array</a>&lt;T&gt;","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='postgres_array/struct.Dimension.html' title='postgres_array::Dimension'>Dimension</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
