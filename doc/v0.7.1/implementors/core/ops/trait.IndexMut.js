56d94bee284e15c553696e310e71fa279026ae2a
Steven Fackler <sfackler@gmail.com>
2016-11-05 22:53:12 -0700

    Release v0.7.1

---
+++ doc/v0.7.1/implementors/core/ops/trait.IndexMut.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["postgres_array"] = ["impl&lt;T,&nbsp;I:&nbsp;<a class='trait' href='postgres_array/array/trait.ArrayIndex.html' title='postgres_array::array::ArrayIndex'>ArrayIndex</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.IndexMut.html' title='core::ops::IndexMut'>IndexMut</a>&lt;I&gt; for <a class='struct' href='postgres_array/array/struct.Array.html' title='postgres_array::array::Array'>Array</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/v0.7.1/implementors/postgres/types/trait.FromSql.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["postgres_array"] = ["impl&lt;T&gt; <a class='trait' href='https://sfackler.github.io/rust-postgres/doc/v0.13.0/postgres/types/trait.FromSql.html' title='postgres::types::FromSql'>FromSql</a> for <a class='struct' href='postgres_array/array/struct.Array.html' title='postgres_array::array::Array'>Array</a>&lt;T&gt; <span class='where'>where T: <a class='trait' href='https://sfackler.github.io/rust-postgres/doc/v0.13.0/postgres/types/trait.FromSql.html' title='postgres::types::FromSql'>FromSql</a></span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
